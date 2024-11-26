de61209339ee50b6aa5e8bb4fcf0b8ca5fba4ec2
moaad007 <147991720+moaad007@users.noreply.github.com>
2024-11-26 17:02:17 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Select all anchor tags with the class "nav-link"
+    const links = document.querySelectorAll('.nav-link');
+    const contentDiv=document.querySelector('.content')
+
+    // Add click event listener to each link
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();  // Prevent default anchor behavior
+
+            // Remove the 'active' class from any currently active links
+            document.querySelectorAll('.nav-link.active').forEach(activeLink => {
+                activeLink.classList.remove('active');
+            });
+
+            // Add the 'active' class to the clicked link
+            this.classList.add('active');
+            const content = this.getAttribute('data-section');
+            RenderContent(content);
+        });
+    });
+    
+    function RenderContent(content){
+    fetch(content)
+    .then(Response=>Response.text())
+    .then(data=>{
+        contentDiv.innerHTML=data;
+    })
+    .catch(err=>{
+        contentDiv.innerHTML= '<p>error 404</p>'
+    })
+
+   
+} RenderContent('pages/dash.html');
+});
