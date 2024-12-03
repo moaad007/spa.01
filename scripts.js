6e15c3e9e7b178da5c0bd7fe07fdd8967e5f38db
moaad007 <147991720+moaad007@users.noreply.github.com>
2024-12-03 16:48:52 +0100

    1.2.3

--- scripts.js
+++ scripts.js
@@ -2,7 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Select all anchor tags with the class "nav-link"
     const links = document.querySelectorAll('.nav-link');
     const contentDiv=document.querySelector('.content')
-
+    const hamMenu = document.querySelector('.hamMenu');
+    const nav = document.querySelector('.nav');
+// Add click event listener to ham menu
+    hamMenu.addEventListener('click',()=>{
+        hamMenu.classList.toggle("active")
+        nav.classList.toggle("active")
+    })
     // Add click event listener to each link
     links.forEach(link => {
         link.addEventListener('click', function (e) {
@@ -11,6 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
             // Remove the 'active' class from any currently active links
             document.querySelectorAll('.nav-link.active').forEach(activeLink => {
                 activeLink.classList.remove('active');
+                hamMenu.classList.remove('active');
+                nav.classList.remove('active');
+
             });
 
             // Add the 'active' class to the clicked link
@@ -18,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const content = this.getAttribute('data-section');
             RenderContent(content);
         });
+        
     });
     
     function RenderContent(content){
@@ -32,4 +42,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
    
 } RenderContent('pages/dash.html');
+
 });
